40063fe5000d4d8cfcd6ef7345d462f475bbff15
Bob Marley <bob@marley.com>
2018-05-04 11:23:50 +0200

    Fonction XHR

---
+++ functions/xhrFunction.js
@@ -0,0 +1,24 @@
+function xhrFunction() {
+    var xhr = null;
+
+    if (window.XMLHttpRequest || window.ActiveXObject) {
+        if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch(e) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        }
+
+        else {
+            xhr = new XMLHttpRequest();
+        }
+    }
+
+    else {
+        return false;
+    }
+
+    return xhr;
+}
