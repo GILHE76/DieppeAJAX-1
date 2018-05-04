63b01f0c50cf89b7c97b6e2be8914e26b5dcc962
Bob Marley <bob@marley.com>
2018-05-04 11:24:12 +0200

    Fichier PHP réponse pour test

---
+++ reponse.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-Type: text/plain");
+$toto1 = isset($_GET['parametre1']) ? $_GET['parametre1'] : "";
+$toto2 = isset($_GET['parametre2']) ? $_GET['parametre2'] : "";
+
+if ($toto1 != "" && $toto2 != "") {
+    echo "YYeeeessssssssssssss !!!!!!!!";
+}
+
+else {
+    echo "Boulay !";
+}
